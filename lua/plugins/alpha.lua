6774ccb025969700adf2b461d9c30071913a459f
Felipe Luiz Wlodkowski <57642609+wlodfelipe@users.noreply.github.com>
2026-02-12 16:45:19 -0300

    Add files via upload

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,31 +1,30 @@
-return {
-  "goolord/alpha-nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
+local alpha = require('alpha')
+local dashboard = require("alpha.themes.dashboard")
+dashboard.section.header.val = {
 
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.startify")
 
-    dashboard.section.header.val = {
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                     ]],
-      [[       ████ ██████           █████      ██                     ]],
-      [[      ███████████             █████                             ]],
-      [[      █████████ ███████████████████ ███   ███████████   ]],
-      [[     █████████  ███    █████████████ █████ ██████████████   ]],
-      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-    }
-
-    alpha.setup(dashboard.opts)
-  end,
+	[[  ^  ^  ^   ^☆ ★ ☆ ___I_☆ ★ ☆ ^  ^   ^  ^  ^   ^  ^ ]],
+	[[ /|\/|\/|\ /|\ ★☆ /\-_--\ ☆ ★/|\/|\ /|\/|\/|\ /|\/|\ ]],
+	[[ /|\/|\/|\ /|\ ★ /  \_-__\☆ ★/|\/|\ /|\/|\/|\ /|\/|\ ]],
+	[[ /|\/|\/|\ /|\ 󰻀 |[]| [] | 󰻀 /|\/|\ /|\/|\/|\ /|\/|\ ]],
 }
+
+dashboard.section.buttons.val = {
+	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("f", "󰍉  Find file", ":lua require('fzf-lua').files() <CR>"),
+	dashboard.button("t", "  Browse cwd", ":NvimTreeOpen<CR>"),
+	dashboard.button("r", "  Browse src", ":e ~/.local/src/<CR>"),
+	dashboard.button("s", "󰯂  Browse scripts", ":e ~/scripts/<CR>"),
+	dashboard.button("c", "  Config", ":e ~/.config/nvim/<CR>"),
+	dashboard.button("m", "  Mappings", ":e ~/.config/nvim/lua/config/mappings.lua<CR>"),
+	dashboard.button("p", "  Plugins", ":PlugInstall<CR>"),
+	dashboard.button("q", "󰅙  Quit", ":q!<CR>"),
+}
+
+dashboard.section.footer.val = function()
+  return vim.g.startup_time_ms or "[[  ]]"
+end
+
+dashboard.section.buttons.opts.hl = "Keyword"
+dashboard.opts.opts.noautocmd = true
+alpha.setup(dashboard.opts)
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,20 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
---      ensure_installed = { "python", "java", "typescript", "javascript", "json", "go", "sql", "bash", "html", "css" },
-      auto_install = true,
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },  
+  config = function ()
+    local treesitter = require("nvim-treesitter")
+    treesitter.setup()
+
+	treesitter.install = { "bash", "c", "css", "cpp", "go", "html", "java", "javascript", "json", "lua", "markdown", "markdown_inline", "python", "rust", "tsx", "typescript" },
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { "bash", "c", "css", "cpp", "go", "html", "java", "javascript", "json", "lua", "markdown", "markdown_inline", "python", "rust", "tsx", "typescript" },
+
+      callback = function()
+        vim.treesitter.start()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
   end
-}
+ }
