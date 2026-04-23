926674efd07859aefe4c7e6484579462c1e502ab
wlodfelipe <wlodfelipe@gmail.com>
2026-04-23 19:29:25 -0300

    added clang lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -47,6 +47,9 @@ return {
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
+	  lspconfig.clangd.setup({
+	    capabilities = capabilities
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
